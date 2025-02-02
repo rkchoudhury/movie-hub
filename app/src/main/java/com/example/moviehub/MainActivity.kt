7460e76d9fb8a42468211ca817a633072895eb5c
Rakesh Choudhury <Rakesh.Choudhury@gds.ey.com>
2025-02-02 19:06:44 +0530

    Added navigation to the project

--- app/src/main/java/com/example/moviehub/MainActivity.kt
+++ app/src/main/java/com/example/moviehub/MainActivity.kt
@@ -3,7 +3,10 @@ package com.example.moviehub
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.moviehub.screens.HomeScreen
 import com.example.moviehub.screens.ProfileSelectionScreen
 import com.example.moviehub.screens.SearchScreen
@@ -14,12 +17,19 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MovieHubTheme {
-                Column {
-                    ProfileSelectionScreen()
-                    HomeScreen()
-                    SearchScreen()
-                }
+                AppNavigation()
             }
         }
+    }
+}
+
+@Composable
+fun AppNavigation() {
+    val navController = rememberNavController()
+
+    NavHost(navController = navController, startDestination = "Profile") {
+        composable("Profile") { ProfileSelectionScreen(navController) }
+        composable("Home") { HomeScreen() }
+        composable("Search") { SearchScreen() }
     }
 }
