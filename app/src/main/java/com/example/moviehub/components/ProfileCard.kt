e765c3537a242a409c0759085a382db45ca674b8
Rakesh Choudhury <Rakesh.Choudhury@gds.ey.com>
2025-02-09 10:16:33 +0530

    Refined the ProfileCard UI

--- app/src/main/java/com/example/moviehub/components/ProfileCard.kt
+++ app/src/main/java/com/example/moviehub/components/ProfileCard.kt
@@ -1,12 +1,15 @@
 package com.example.moviehub.components
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -22,23 +25,47 @@ fun ProfileCard(name: String, type: String) {
     Row(
         modifier = Modifier
             .fillMaxWidth()
-            .background(color = colorResource(R.color.grey))
+            .padding(horizontal = 0.dp, vertical = 10.dp)
+            .background(color = colorResource(R.color.black_two), shape = RoundedCornerShape(20.dp))
             .padding(24.dp)
     ) {
-        Column {
+        Text(
+            text = name[0].toString(),
+            color = colorResource(R.color.white),
+            fontSize = 32.sp,
+            fontWeight = FontWeight.SemiBold,
+            modifier = Modifier
+                .background(
+                    color = colorResource(R.color.gold),
+                    shape = RoundedCornerShape(10.dp)
+                )
+                .padding(15.dp, 8.dp)
+        )
+        Spacer(modifier = Modifier.width(20.dp))
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(0.dp, 6.dp),
+            verticalArrangement = Arrangement.Center
+        ) {
             Text(
                 text = name,
                 color = colorResource(R.color.white_two),
-                fontSize = 20.sp,
+                fontSize = 18.sp,
                 fontWeight = FontWeight.SemiBold,
             )
             Spacer(modifier = Modifier.height(2.dp))
-            Text(text = type, color = colorResource(R.color.grey_two), fontSize = 16.sp)
+            Text(
+                text = type,
+                color = colorResource(R.color.grey_two),
+                fontSize = 16.sp,
+                fontWeight = FontWeight.Medium
+            )
         }
     }
 }
 
-@Preview(showSystemUi = true)
+@Preview(showSystemUi = false)
 @Composable
 fun ProfileCardPreview() {
     ProfileCard("Rakesh", "Adult")
