fab1d6aa48670504b044cc58559f62acbf777622
Rakesh Choudhury <Rakesh.Choudhury@gds.ey.com>
2025-02-15 11:51:11 +0530

    Added MoviePreviewCard and called from the home screen

---
+++ app/src/main/java/com/example/moviehub/components/MoviePreviewCard.kt
@@ -0,0 +1,15 @@
+package com.example.moviehub.components
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun MoviePreviewCard() {
+
+}
+
+@Preview
+@Composable
+fun MoviePreviewCardPreview() {
+    MoviePreviewCard()
+}
--- app/src/main/java/com/example/moviehub/screens/dashboard/HomeScreen.kt
+++ app/src/main/java/com/example/moviehub/screens/dashboard/HomeScreen.kt
@@ -1,7 +1,7 @@
 package com.example.moviehub.screens.dashboard
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -13,6 +13,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.moviehub.R
 import com.example.moviehub.components.MovieList
+import com.example.moviehub.components.MoviePreviewCard
 import com.example.moviehub.models.MovieState
 import com.example.moviehub.viewmodels.MovieCollectionViewModel
 import com.example.moviehub.viewmodels.MovieViewModel
@@ -26,18 +27,22 @@ fun HomeScreen() {
     val topRatedMoviesState by movieCollectionViewModel.topRatedMoviesState
     val nowPlayingMoviesState by movieCollectionViewModel.nowPlayingMoviesState
 
-    Box(
+    Column(
         modifier = Modifier
             .fillMaxSize()
             .background(color = colorResource(R.color.grey))
     ) {
+        if (nowPlayingMoviesState.list.isNotEmpty()) {
+            val latestMovie = nowPlayingMoviesState.list[0]
+            MoviePreviewCard(latestMovie)
+        }
         LazyColumn {
             items(
                 listOf<MovieState>(
-                    upcomingMoviesState,
-                    popularMovieState,
                     nowPlayingMoviesState,
-                    topRatedMoviesState
+                    popularMovieState,
+                    topRatedMoviesState,
+                    upcomingMoviesState,
                 )
             ) { item ->
                 MovieList(item.list, item.loading, item.error, item.categoryTitle)
