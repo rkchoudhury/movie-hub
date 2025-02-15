0990af3341cd2093bc7d0636708a0dfc37545ec1
Rakesh Choudhury <Rakesh.Choudhury@gds.ey.com>
2025-02-15 09:43:25 +0530

    Updated bottom bar UI

--- app/src/main/java/com/example/moviehub/components/BottomTabBar.kt
+++ app/src/main/java/com/example/moviehub/components/BottomTabBar.kt
@@ -1,12 +1,16 @@
 package com.example.moviehub.components
 
+import androidx.compose.foundation.background
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
-import androidx.compose.material3.Text
+import androidx.compose.material3.NavigationBarItemColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
 import androidx.navigation.NavController
 import androidx.navigation.compose.currentBackStackEntryAsState
+import com.example.moviehub.R
 import com.example.moviehub.navigation.DashboardTabItem
 
 @Composable
@@ -19,10 +23,19 @@ fun BottomTabBar(
     NavigationBar {
         navigationItems.forEachIndexed { index, item ->
             NavigationBarItem(
-                alwaysShowLabel = true,
+                alwaysShowLabel = false,
                 icon = { Icon(item.icon, contentDescription = item.title) },
-                label = { Text(item.title) },
                 selected = item.route == backStackEntry.value?.destination?.route,
+                colors = NavigationBarItemColors(
+                    selectedIconColor = colorResource(R.color.gold),
+                    selectedTextColor = colorResource(R.color.gold),
+                    unselectedIconColor = colorResource(R.color.white_two),
+                    unselectedTextColor = colorResource(R.color.white_two),
+                    selectedIndicatorColor = colorResource(R.color.grey),
+                    disabledIconColor = colorResource(R.color.grey_two),
+                    disabledTextColor = colorResource(R.color.grey_two)
+                ),
+                modifier = Modifier.background(color = colorResource(R.color.grey)),
                 onClick = {
                     navController.navigate(item.route) {
                         navController.graph.startDestinationRoute?.let { route ->
--- app/src/main/java/com/example/moviehub/screens/dashboard/DashboardNavigator.kt
+++ app/src/main/java/com/example/moviehub/screens/dashboard/DashboardNavigator.kt
@@ -1,10 +1,13 @@
 package com.example.moviehub.screens.dashboard
 
 import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.height
 import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.rememberNavController
 import com.example.moviehub.components.BottomTabBar
 import com.example.moviehub.navigation.DashboardTabItem
@@ -23,7 +26,10 @@ fun DashboardNavigator() {
 
     Scaffold(
         bottomBar = {
-            BottomAppBar(modifier = Modifier) {
+            BottomAppBar(
+                modifier = Modifier.height(50.dp),
+                contentPadding = PaddingValues(0.dp),
+            ) {
                 BottomTabBar(
                     navController = navController,
                     navigationItems = navigationItems,
