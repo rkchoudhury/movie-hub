db6295c66becbe85a23a5f664862a3732802e395
Rakesh Choudhury <Rakesh.Choudhury@gds.ey.com>
2025-03-10 17:59:26 +0530

    Added NavigationBar Component

---
+++ app/src/main/java/com/example/moviehub/components/NavigationBar.kt
@@ -0,0 +1,54 @@
+package com.example.moviehub.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.rounded.ArrowBack
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.moviehub.R
+
+@Composable
+fun NavigationBar(title: String) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(50.dp)
+            .background(colorResource(R.color.grey))
+            .padding(horizontal = 5.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Icon(
+            imageVector = Icons.AutoMirrored.Rounded.ArrowBack,
+            contentDescription = "back_button",
+            tint = colorResource(R.color.gold)
+        )
+        Text(
+            text = title,
+            modifier = Modifier
+                .fillMaxWidth(),
+            textAlign = TextAlign.Center,
+            fontSize = 16.sp,
+            fontWeight = FontWeight.Bold,
+            color = colorResource(R.color.gold),
+        )
+    }
+}
+
+@Preview(showSystemUi = false, showBackground = true)
+@Composable
+fun PreviewNavigationBar() {
+    NavigationBar("Movie Preview")
+}
--- app/src/main/java/com/example/moviehub/screens/view_all/ViewAllMovieScreen.kt
+++ app/src/main/java/com/example/moviehub/screens/view_all/ViewAllMovieScreen.kt
@@ -1,16 +1,16 @@
 package com.example.moviehub.screens.view_all
 
 import androidx.compose.foundation.layout.Column
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.moviehub.components.MovieGrid
+import com.example.moviehub.components.NavigationBar
 import com.example.moviehub.models.Movie
 
 @Composable
 fun ViewAllMovie(title: String, movieList: List<Movie>) {
     Column {
-        Text(text = title)
+        NavigationBar(title = title)
         MovieGrid(movieList)
     }
 }
