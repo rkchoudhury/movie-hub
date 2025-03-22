884ea0745334500a92cc5966b89ae5b8f3223f17
rkchoudhury <rakeshchoudhury.rocky@gmail.com>
2025-03-22 20:08:14 +0530

    Added underlined text

--- app/src/main/java/com/example/moviehub/screens/view_all/ViewAllMovieScreen.kt
+++ app/src/main/java/com/example/moviehub/screens/view_all/ViewAllMovieScreen.kt
@@ -14,6 +14,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -74,7 +75,8 @@ fun LoadMoreCard(
         Text(
             text = if (moreMoviesState.loading) "Loading..." else "Load More",
             color = colorResource(R.color.gold),
-            fontWeight = FontWeight.Bold
+            fontWeight = FontWeight.Bold,
+            textDecoration = TextDecoration.Underline
         )
     }
 }
